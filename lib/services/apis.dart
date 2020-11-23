3fafafa828fde80abb098be7db8bfb060da84205
ashikajeeb <ashik.ajeeb@zartek.in>
2020-11-23 12:13:19 +0530

    first commit

--- lib/services/apis.dart
+++ lib/services/apis.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
-
+//Api
 class Apis {
   String BASE_URL = "https://zartekpms.herokuapp.com/";
   // login
